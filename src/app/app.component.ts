ffd6b7b7f37a8fd09418031dba3831293369cee5
zeinab <zeinabibrahim592@gmail.com>
2024-03-02 12:05:50 +0200

    work on project dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] // Note: Correct property name is styleUrls, not styleUrl
 })
 export class AppComponent {
   title = 'team-task-management';
+
+  selectedValue = 'team'; // Default value is 'team'
+
+  // Method to handle navigation changes
+  onNavigate(value: string) {
+    this.selectedValue = value;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectDashboardComponent } from './project-dashboard/project-dashboard.component';
+import { HeaderComponent } from './header/header.component';
+import { TeamComponent } from './team/team.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectDashboardComponent,
+    HeaderComponent,
+    TeamComponent
   ],
   imports: [
     BrowserModule,
